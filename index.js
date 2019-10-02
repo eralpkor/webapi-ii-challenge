f72b7a123d6aa3ead4e4b9e0c9bfe3b1dded6340
eralpkor <eralp.ny@gmail.com>
2019-10-02 10:11:32 -0400

    added cors() to index.js sor stretch

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const server = express();
 const blogRouter = require('./router/router.js');
+var cors = require('cors');
+
 
 server.use(express.json());
 // server to use endpoints from router
 server.use('/api/posts', blogRouter);
+server.use(cors());
+
 
 // run server on '/'
 server.get('/', (req, res) => {
